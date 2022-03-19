b9be8f5849bc140a6242836d8fb0c97492e1d15a
Darian Benam <darianbenam@hotmail.com>
2022-03-19 11:45:08 -0400

    Switched from ASP.NET Razor to ASP.NET Web API

--- sprint-compass-backend/Startup.cs
+++ sprint-compass-backend/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace SprintCompass_Group4
+namespace SprintCompassBackend
 {
     public class Startup
     {
@@ -23,7 +18,7 @@ namespace SprintCompass_Group4
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,15 +28,8 @@ namespace SprintCompass_Group4
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -49,7 +37,7 @@ namespace SprintCompass_Group4
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
